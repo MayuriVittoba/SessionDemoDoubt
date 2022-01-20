71b04c273a17fc5d94ee904c917a582a71f11feb
MayuriVittoba <92469986+MayuriVittoba@users.noreply.github.com>
2022-01-20 21:29:55 +0530

    Session demo simplilearn doubt

---
+++ SessionDemo1/src/main/java/dto/User.java
@@ -0,0 +1,65 @@
+package dto;
+
+public class User {
+	private String fname, lname, email, city;
+	private int age;
+	private long contact;
+
+	public User() {
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	@Override
+	public String toString() {
+		return "User [fname=" + fname + ", lname=" + lname + ", email=" + email + ", city=" + city + ", age=" + age
+				+ ", contact=" + contact + "]";
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public long getContact() {
+		return contact;
+	}
+
+	public void setContact(long contact) {
+		this.contact = contact;
+	}
+
+}
